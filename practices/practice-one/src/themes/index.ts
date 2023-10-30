b4dffe1e2f597dac4b04f5b41d19aa8e4e20f59d
duong.pham <duong.pham@asnet.com.vn>
2023-10-30 16:14:59 +0700

    Custom for Container

--- practices/practice-one/src/themes/index.ts
+++ practices/practice-one/src/themes/index.ts
@@ -2,12 +2,13 @@ import { extendTheme } from '@chakra-ui/react';
 
 // Base themes
 import * as baseThemes from './base';
-import { button, text } from './components';
+import { button, text, container } from './components';
 
 export const overrideThemes = extendTheme({
   ...baseThemes,
   components: {
     ...button,
     ...text,
+    ...container,
   },
 });
