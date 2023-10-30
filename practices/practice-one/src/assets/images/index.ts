65a96cb306932391b47df7250f43546e9faea9c3
duong.pham <duong.pham@asnet.com.vn>
2023-10-30 13:54:52 +0700

    Add images

---
+++ practices/practice-one/src/assets/images/index.ts
@@ -0,0 +1,6 @@
+export { default as CardSkin1 } from './card-skin-1.png';
+export { default as CardSkin2 } from './card-skin-2.png';
+export { default as CardSkin3 } from './card-skin-3.png';
+export { default as CardSkin4 } from './card-skin-4.png';
+export { default as CardSkin5 } from './card-skin-5.png';
+export { default as CardSkin6 } from './card-skin-6.png';
