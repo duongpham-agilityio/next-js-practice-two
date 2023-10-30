69335144eb50f4533a7efe48095b79246da63d0d
duong.pham <duong.pham@asnet.com.vn>
2023-10-30 13:55:06 +0700

    Add icons

---
+++ practices/practice-one/src/assets/icons/Copy.tsx
@@ -0,0 +1,18 @@
+import { SVGProps, memo } from 'react';
+
+const CopyComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={15}
+    height={16}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#C9CED6"
+      d="M13.125 12.125H7.5a1.851 1.851 0 0 1-1.875-1.875v-7.5A1.87 1.87 0 0 1 7.5.875h4.102c.351 0 .732.176.996.44l1.962 1.962c.264.264.44.645.44.996v5.977a1.87 1.87 0 0 1-1.875 1.875Zm-11.25-7.5h2.813v1.406H1.874a.482.482 0 0 0-.469.469V14c0 .264.205.469.469.469H7.5A.482.482 0 0 0 7.969 14v-.938h1.406V14A1.87 1.87 0 0 1 7.5 15.875H1.875A1.851 1.851 0 0 1 0 14V6.5a1.87 1.87 0 0 1 1.875-1.875Z"
+    />
+  </svg>
+);
+
+export const CopyIcon = memo(CopyComponent);
---
+++ practices/practice-one/src/assets/icons/Triangle.tsx
@@ -0,0 +1,34 @@
+import { SVGProps, memo } from 'react';
+
+const TriangleDownComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={7}
+    height={6}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#C9CED6"
+      d="M3.91 5.273 6.513.838a.49.49 0 0 0 0-.483.457.457 0 0 0-.41-.24H.897a.462.462 0 0 0-.421.24.49.49 0 0 0 0 .483l2.603 4.435a.481.481 0 0 0 .832 0Z"
+    />
+  </svg>
+);
+
+const TriangleUpComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={7}
+    height={7}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#C9CED6"
+      d="m3.91 1.227 2.603 4.435a.49.49 0 0 1 0 .483.457.457 0 0 1-.41.24H.897a.462.462 0 0 1-.421-.24.49.49 0 0 1 0-.482l2.603-4.436a.481.481 0 0 1 .832 0Z"
+    />
+  </svg>
+);
+
+export const TriangleDown = memo(TriangleDownComponent);
+export const TriangleUp = memo(TriangleUpComponent);
