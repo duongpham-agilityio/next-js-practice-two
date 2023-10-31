5e6bcbced8f4244bf5decefd842ba4c07ac5a196
duong.pham <duong.pham@asnet.com.vn>
2023-10-31 10:27:06 +0700

    Add mocks table head

---
+++ practices/practice-one/src/mocks/table.ts
@@ -0,0 +1,32 @@
+// Components
+import { TTableHeadCell } from '@components/Table/TableHead';
+
+export const tableHead: TTableHeadCell[] = [
+  {
+    title: 'Card number',
+  },
+
+  {
+    title: 'Card name',
+  },
+
+  {
+    title: 'Card image',
+  },
+
+  {
+    title: 'Status',
+  },
+
+  {
+    title: 'Created time',
+  },
+
+  {
+    title: 'Updated time',
+  },
+
+  {
+    title: 'Actions',
+  },
+];
