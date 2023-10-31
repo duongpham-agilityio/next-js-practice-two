17d764c5b0942c6bfab96f4d16721c6abd79ab98
duong.pham <duong.pham@asnet.com.vn>
2023-10-31 14:38:39 +0700

    Create data mock for tableBody

--- practices/practice-one/src/mocks/table.ts
+++ practices/practice-one/src/mocks/table.ts
@@ -30,3 +30,45 @@ export const tableHead: TTableHeadCell[] = [
     title: 'Actions',
   },
 ];
+
+type CardInfoMock = {
+  number: string;
+  name: string;
+  skin: number;
+  status: 'active' | 'inactive' | 'close';
+  role: 'admin' | 'user';
+  createdAt: string;
+  updatedAt: string;
+};
+
+export const tableBody: CardInfoMock[] = [
+  {
+    number: '123-456-789-000',
+    name: 'John Smith',
+    skin: 0,
+    status: 'active',
+    role: 'admin',
+    createdAt: '12:00 AM Oct - 10 -2023',
+    updatedAt: '12:00 AM Oct - 10 -2023',
+  },
+
+  {
+    number: '123-456-789-000',
+    name: 'John Smith',
+    skin: 0,
+    status: 'inactive',
+    role: 'user',
+    createdAt: '12:00 AM Oct - 10 -2023',
+    updatedAt: '12:00 AM Oct - 10 -2023',
+  },
+
+  {
+    number: '123-456-789-000',
+    name: 'John Smith',
+    skin: 0,
+    status: 'close',
+    role: 'user',
+    createdAt: '12:00 AM Oct - 10 -2023',
+    updatedAt: '12:00 AM Oct - 10 -2023',
+  },
+];
