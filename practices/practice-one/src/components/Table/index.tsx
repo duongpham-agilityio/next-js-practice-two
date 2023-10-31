674ea50c7205bda4b1f37886d2a234a868da7853
duong.pham <duong.pham@asnet.com.vn>
2023-10-31 14:38:11 +0700

    Create Table

--- practices/practice-one/src/components/Table/index.tsx
+++ practices/practice-one/src/components/Table/index.tsx
@@ -1,11 +1,15 @@
 import { TableContainer, Table as TableChakra } from '@chakra-ui/react';
 import { memo } from 'react';
+
+// Components
 import TableHead from './TableHead';
+import TableBody from './TableBody';
 
 const Table = (): JSX.Element => (
   <TableContainer w="full" mt={8}>
-    <TableChakra>
+    <TableChakra style={{ borderCollapse: 'separate', borderSpacing: '0 3px' }}>
       <TableHead />
+      <TableBody />
     </TableChakra>
   </TableContainer>
 );
