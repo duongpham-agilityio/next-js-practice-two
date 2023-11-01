29cd74fda20181f5d2ab205b9a9bf50780079e1e
duong.pham <duong.pham@asnet.com.vn>
2023-11-01 10:04:40 +0700

    Add comment for helper

--- practices/practice-one/src/helpers/date.ts
+++ practices/practice-one/src/helpers/date.ts
@@ -3,6 +3,11 @@ type TTime = {
   suffix: string;
 };
 
+/**
+ *  Handle get hour format
+ * @param hours
+ * @returns (01, 02,...)
+ */
 const getHourString = (hours: number): TTime => {
   const times = Array.from({ length: 24 }).map((_, index: number) => {
     const time: string = index < 10 ? `0${index}` : `${index}`;
@@ -17,6 +22,11 @@ const getHourString = (hours: number): TTime => {
   return times[hours];
 };
 
+/**
+ *  Handle get day minute
+ * @param minute
+ * @returns (01, 02,...)
+ */
 const getMinuteString = (minute: number): string => {
   const times = Array.from({ length: 60 }).map((_, index: number) =>
     index < 10 ? `0${index}` : `${index}`,
@@ -44,6 +54,11 @@ const getMonthString = (month: number): string => {
   return months[month];
 };
 
+/**
+ * Handle get day format
+ * @param date
+ * @returns (01, 02,...)
+ */
 const getDateString = (date: number): string => {
   const dates: string[] = Array.from({ length: 30 }).map((_, index: number) =>
     index < 10 ? `0${index + 1}` : `${index}`,
@@ -52,6 +67,11 @@ const getDateString = (date: number): string => {
   return dates[date - 1];
 };
 
+/**
+ * Format date
+ * @param date
+ * @returns  "hh:mm (AM | PM) month - day - year"
+ */
 export const getTimeString = (date: Date) => {
   const hours: TTime = getHourString(date.getHours());
   const minute: string = getMinuteString(date.getMinutes());
