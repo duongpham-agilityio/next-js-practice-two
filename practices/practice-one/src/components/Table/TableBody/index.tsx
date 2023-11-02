6c8f366f620ca4481cdf18cd6be7c2db9aa2c1cd
duong.pham <duong.pham@asnet.com.vn>
2023-11-02 16:23:56 +0700

    Handle active, inactive, close card

--- practices/practice-one/src/components/Table/TableBody/index.tsx
+++ practices/practice-one/src/components/Table/TableBody/index.tsx
@@ -1,20 +1,221 @@
 'use client';
 
-import { Tbody } from '@chakra-ui/react';
-import { memo, useCallback } from 'react';
+import { Tbody, useDisclosure, useToast } from '@chakra-ui/react';
+import { memo, useCallback, useState } from 'react';
+import { useQueryClient } from '@tanstack/react-query';
+
+// Hooks
+import { useCard } from '@hooks/index';
 
 // Helpers
 import { getTimeString } from '@helpers/date';
 
-// Icons
+// Constants
+import { ROUTES } from '@constants/url';
+import { MESSAGES } from '@constants/messages';
+import { TITLES } from '@constants/titles';
+
+// Components
 import Row from './components/Row';
+
+// Types
 import { ICard } from '@interfaces/card';
+import Dialog from '@components/Dialog';
 
 export type TableBodyProps = {
   data: ICard[];
 };
+type TStatusInfo = {
+  title: string;
+  desc: string;
+};
+type TStatus = Record<ICard['status'], TStatusInfo>;
 
 const TableBody = ({ data }: TableBodyProps): JSX.Element => {
+  const [idIsSelected, setIdIsSelected] = useState('');
+  const { isOpen: isOpenDialog, onToggle } = useDisclosure();
+  const queryClient = useQueryClient();
+  const { activeHandler, inactiveHandler, closeHandler } = useCard();
+  const toast = useToast({
+    duration: 2000,
+  });
+
+  /**
+   * Handle get card in list card
+   * @param id the ID of card item
+   * @returns card
+   */
+  const getCardById = useCallback(
+    (id: string): ICard | undefined => data.find((item) => item.id === id),
+    [data],
+  );
+
+  /**
+   * Handle get info of warning status
+   * @param status status of card
+   * @returns info status
+   */
+  const getWarningInfo = useCallback((status: ICard['status']): TStatusInfo => {
+    const info: TStatus = {
+      active: {
+        title: TITLES.ACTIVE,
+        desc: MESSAGES.ACTIVE_FAILED,
+      },
+      inactive: {
+        title: TITLES.INACTIVE,
+        desc: MESSAGES.INACTIVE_FAILED,
+      },
+      close: {
+        title: TITLES.CLOSE,
+        desc: MESSAGES.CLOSE_CARD_FAILED,
+      },
+    };
+
+    return info[status];
+  }, []);
+
+  /**
+   * Check if the state to be updated is the same as the original state
+   * @param id The ID of the card needs check
+   * @param status Status you want to update
+   * @returns
+   */
+  const isFieldUpdateCorrect = useCallback(
+    (id: string, status: ICard['status']): boolean => {
+      const card: ICard | undefined = getCardById(id);
+
+      if (!card) return false;
+
+      const { status: stt } = card;
+
+      if (stt === status) {
+        const { title, desc: description }: TStatusInfo =
+          getWarningInfo(status);
+
+        toast({
+          title,
+          description,
+          status: 'warning',
+        });
+
+        return false;
+      }
+
+      return true;
+    },
+    [getCardById, getWarningInfo, toast],
+  );
+
+  /**
+   * Common error handling when performing unsuccessful actions
+   * @param title The title match the current action
+   * @returns The callback error handler
+   */
+  const errorHandler = useCallback(
+    (title: string): ((error: Error) => void) =>
+      (error: Error) =>
+        toast({
+          title: title,
+          description: error.message,
+          status: 'error',
+        }),
+    [toast],
+  );
+
+  /**
+   * Common success handling when performing successful actions
+   * @param title The title match the current action
+   * @param description The message show on screen
+   * @returns The callback success handler
+   */
+  const successHandler = useCallback(
+    (title: string, description: string): (() => void) =>
+      () => {
+        toast({
+          title,
+          description,
+          status: 'success',
+        });
+
+        queryClient.invalidateQueries({ queryKey: [ROUTES.CARD] });
+      },
+    [queryClient, toast],
+  );
+
+  /**
+   * Handle active card
+   * @param id
+   */
+  const handleActiveCard = useCallback(
+    (id: string): void => {
+      const isCorrect: boolean = isFieldUpdateCorrect(id, 'active');
+
+      if (!isCorrect) return;
+
+      activeHandler(
+        id,
+        successHandler(TITLES.ACTIVE, MESSAGES.ACTIVE_SUCCESS),
+        errorHandler(TITLES.ACTIVE),
+      );
+    },
+    [activeHandler, errorHandler, isFieldUpdateCorrect, successHandler],
+  );
+
+  /**
+   * Handle inactive card
+   * @param id
+   */
+  const handleInActiveCard = useCallback(
+    (id: string): void => {
+      const isCorrect: boolean = isFieldUpdateCorrect(id, 'inactive');
+
+      if (!isCorrect) return;
+
+      inactiveHandler(
+        id,
+        successHandler(TITLES.INACTIVE, MESSAGES.INACTIVE_SUCCESS),
+        errorHandler(TITLES.INACTIVE),
+      );
+    },
+    [errorHandler, inactiveHandler, isFieldUpdateCorrect, successHandler],
+  );
+
+  /**
+   * Handle close card
+   * @param id
+   */
+  const handleCloseCard = useCallback(async (): Promise<void> => {
+    const isCorrect: boolean = isFieldUpdateCorrect(idIsSelected, 'close');
+
+    if (!isCorrect) return;
+
+    await closeHandler(
+      idIsSelected,
+      successHandler(TITLES.CLOSE, MESSAGES.CLOSE_CARD_SUCCESS),
+      errorHandler(TITLES.CLOSE),
+    );
+    onToggle();
+  }, [
+    closeHandler,
+    errorHandler,
+    idIsSelected,
+    isFieldUpdateCorrect,
+    onToggle,
+    successHandler,
+  ]);
+
+  /**
+   *
+   * @param id
+   */
+  const handleSelectedId = useCallback(
+    (id: string): void => {
+      setIdIsSelected(id);
+      onToggle();
+    },
+    [onToggle],
+  );
+
   /**
    * Handle navigate to detail page
    * @param path
@@ -33,54 +234,37 @@ const TableBody = ({ data }: TableBodyProps): JSX.Element => {
     console.log(cardNumber);
   }, []);
 
-  /**
-   * Handle active card
-   * @param id
-   */
-  const handleActiveCard = useCallback((id: string): void => {
-    // TODO: Update to later
-    console.log(id);
-  }, []);
-
-  /**
-   * Handle inactive card
-   * @param id
-   */
-  const handleInActiveCard = useCallback((id: string): void => {
-    // TODO: Update to later
-    console.log(id);
-  }, []);
-
-  /**
-   * Handle close card
-   * @param id
-   */
-  const handleCloseCard = useCallback((id: string): void => {
-    // TODO: Update to later
-    console.log(id);
-  }, []);
-
   return (
-    <Tbody>
-      {data.map((cell): JSX.Element => {
-        const createdAt: string = getTimeString(new Date(cell.createdAt));
-        const updatedAt: string = getTimeString(new Date(cell.updatedAt));
+    <>
+      <Tbody>
+        {data.map((cell): JSX.Element => {
+          const createdAt: string = getTimeString(new Date(cell.createdAt));
+          const updatedAt: string = getTimeString(new Date(cell.updatedAt));
 
-        return (
-          <Row
-            key={cell.id}
-            {...cell}
-            createdAt={createdAt}
-            updatedAt={updatedAt}
-            onCopy={handleCopyCardNumber}
-            onActive={handleActiveCard}
-            onInActive={handleInActiveCard}
-            onClose={handleCloseCard}
-            onNavigate={handleNavigation}
-          />
-        );
-      })}
-    </Tbody>
+          return (
+            <Row
+              key={cell.id}
+              {...cell}
+              createdAt={createdAt}
+              updatedAt={updatedAt}
+              onCopy={handleCopyCardNumber}
+              onActive={handleActiveCard}
+              onInActive={handleInActiveCard}
+              onClose={handleSelectedId}
+              onNavigate={handleNavigation}
+            />
+          );
+        })}
+
+        <Dialog
+          isOpen={isOpenDialog}
+          title={TITLES.CLOSE}
+          description={MESSAGES.DIALOG_CONFIRM}
+          onClose={onToggle}
+          onAccept={handleCloseCard}
+        />
+      </Tbody>
+    </>
   );
 };
 
