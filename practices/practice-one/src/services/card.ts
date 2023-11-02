e1eee73ce0a66226c99981213736ec5a4693175a
duong.pham <duong.pham@asnet.com.vn>
2023-11-02 15:42:11 +0700

    Create service handle update card

--- practices/practice-one/src/services/card.ts
+++ practices/practice-one/src/services/card.ts
@@ -1,4 +1,5 @@
 // Constants
+import { MESSAGES } from '@constants/messages';
 import { ROUTES } from '@constants/url';
 
 // Types
@@ -9,6 +10,35 @@ import { ICard } from '@interfaces/card';
  * @returns
  */
 export const getAllCard = async (): Promise<ICard[]> =>
-  await fetch(`${process.env.NEXT_PUBLIC_DB_URL}/${ROUTES.CARD}`).then((res) =>
-    res.json(),
-  );
+  await fetch(`${process.env.NEXT_PUBLIC_DB_URL}/${ROUTES.CARD}`, {
+    next: { tags: [ROUTES.CARD] },
+    cache: 'no-store',
+  }).then((res) => res.json());
+
+/**
+ * Update card by id
+ * @param id
+ * @param payload
+ * @returns
+ */
+export const updateCard = async (
+  id: string,
+  payload: Partial<ICard>,
+): Promise<ICard> => {
+  const response: ICard = await fetch(
+    `${process.env.NEXT_PUBLIC_DB_URL}/${ROUTES.CARD}/${id}`,
+    {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    },
+  ).then((res) => {
+    if (res.ok) return res.json();
+
+    throw new Error(MESSAGES.FAILED_TO_FETCH);
+  });
+
+  return response;
+};
