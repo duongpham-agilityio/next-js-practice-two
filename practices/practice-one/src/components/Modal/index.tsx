e83dae460a79b7dff6984a31bd6aab205d2be022
duong.pham <duong.pham@asnet.com.vn>
2023-11-03 10:13:28 +0700

    Custom modal component

---
+++ practices/practice-one/src/components/Modal/index.tsx
@@ -0,0 +1,56 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+import { ReactNode, memo } from 'react';
+
+export type TModalProps = {
+  children?: ReactNode;
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const Component = (props: TModalProps): JSX.Element => {
+  const { children, title, ...rest } = props;
+
+  return (
+    <Modal {...rest} isCentered>
+      <ModalOverlay />
+      <ModalContent
+        minW={{
+          base: 'full',
+          md: '70%',
+          lg: '50%',
+        }}
+        h={{
+          base: 'full',
+          md: '70%',
+        }}
+      >
+        <ModalHeader
+          fontSize={{
+            base: 'xl',
+            lg: '5xl',
+          }}
+        >
+          {title}
+        </ModalHeader>
+
+        <ModalCloseButton />
+
+        <ModalBody overflow="hidden" py={5}>
+          {children}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const ModalCustom = memo(Component);
+
+export default ModalCustom;
