3c4be5ef544c727e9729ccc16b7eff07c10686b4
duong.pham <duong.pham@asnet.com.vn>
2023-11-03 13:42:10 +0700

    Create information card component

---
+++ practices/practice-one/src/app/card/[id]/components/Info/index.tsx
@@ -0,0 +1,78 @@
+import { memo } from 'react';
+import { Flex, ListItem, Text, UnorderedList } from '@chakra-ui/react';
+import Image from 'next/image';
+
+// Helpers
+import { getTimeString } from '@helpers/date';
+
+// Mocks
+import { skinCards } from '@mocks/card';
+
+export type InfoCardProps = {
+  skin: number;
+  cardNumber: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+const InfoCard = ({
+  skin,
+  cardNumber,
+  name,
+  createdAt,
+  updatedAt,
+}: InfoCardProps): JSX.Element => {
+  const imageURL: string = skinCards[skin];
+
+  return (
+    <Flex mt={10} gap={45}>
+      <Image src={imageURL} alt="The skin for card" width={351} height={223} />
+
+      <UnorderedList flex={1} fontSize="lg" color="primary">
+        <ListItem pb={2}>
+          <Text>
+            <Text as="span" fontWeight="bold">
+              Card number:
+            </Text>
+            <Text as="span" px={3}>
+              {cardNumber}
+            </Text>
+          </Text>
+        </ListItem>
+        <ListItem py={2}>
+          <Text>
+            <Text as="span" fontWeight="bold">
+              Name:
+            </Text>
+            <Text as="span" px={3}>
+              {name}
+            </Text>
+          </Text>
+        </ListItem>
+        <ListItem py={2}>
+          <Text>
+            <Text as="span" fontWeight="bold">
+              Created at:
+            </Text>
+            <Text as="span" px={3}>
+              {getTimeString(new Date(createdAt))}
+            </Text>
+          </Text>
+        </ListItem>
+        <ListItem py={2}>
+          <Text>
+            <Text as="span" fontWeight="bold">
+              Updated at:
+            </Text>
+            <Text as="span" px={3}>
+              {getTimeString(new Date(updatedAt))}
+            </Text>
+          </Text>
+        </ListItem>
+      </UnorderedList>
+    </Flex>
+  );
+};
+
+export default memo(InfoCard);
