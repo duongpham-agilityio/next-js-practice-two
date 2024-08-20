e4d903765b803e8b005723b149d9fe2a0a7908f6
duong.pham <duong.pham@asnet.com.vn>
2024-08-20 18:10:12 +0700

    fix: update style for BlogCardSkeleton

--- practices/practice-two/features/blog/components/BlogCardSkeleton/index.tsx
+++ practices/practice-two/features/blog/components/BlogCardSkeleton/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Card, Skeleton } from '@nextui-org/react';
 
 export const BlogCardSkeleton = () => (
-  <Card className="w-full min-h-[600px] space-y-5 p-4 flex flex-col">
-    <Skeleton className="rounded-lg">
-      <div className="h-80 rounded-lg bg-default-300" />
+  <Card className="w-full min-h-[600px] space-y-5 flex flex-col rounded-none">
+    <Skeleton>
+      <div className="h-80 bg-default-300" />
     </Skeleton>
-    <div className="flex-1 flex flex-col justify-between space-y-3 pb-10">
+    <div className="flex-1 flex flex-col justify-between space-y-3 px-5 pb-10">
       <Skeleton className="w-3/12 h-3 rounded-lg" />
       <div className="flex-1 space-y-3">
         <Skeleton className="w-full h-3 rounded-lg" />
