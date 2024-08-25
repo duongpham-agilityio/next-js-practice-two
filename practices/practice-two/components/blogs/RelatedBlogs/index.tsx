5ccc496282bf9f47fe10f567147c43d2d6de8b96
duong.pham <duong.pham@asnet.com.vn>
2024-08-25 22:38:34 +0700

    feat: implement UI related blogs

---
+++ practices/practice-two/components/blogs/RelatedBlogs/index.tsx
@@ -0,0 +1,33 @@
+// Components
+import { Button, Container, BlogCard } from '@/components';
+// Constants
+import { ROUTE } from '@/constants';
+// Services
+import { getBlogs } from '@/services';
+
+export const RelatedBlogs = async () => {
+  const blogs = await getBlogs();
+
+  return (
+    <section className="flex flex-col gap-10">
+      <Container
+        as="ul"
+        className="grid grid-cols-[320px,320px,320px,320px] xl:grid-cols-4 gap-5 mt-5 overflow-x-scroll xl:overflow-x-visible"
+      >
+        {blogs.slice(0, 4).map(({ id, author, imageURL, title }) => (
+          <li key={id} className="rounded-xl overflow-hidden">
+            <BlogCard
+              author={author}
+              href={`${ROUTE.BLOGS}/${id}`}
+              imageURL={imageURL}
+              title={title}
+            />
+          </li>
+        ))}
+      </Container>
+      <div className="flex items-center justify-center">
+        <Button>See all post</Button>
+      </div>
+    </section>
+  );
+};
